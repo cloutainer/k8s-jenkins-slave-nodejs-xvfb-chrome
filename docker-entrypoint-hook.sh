6a54c417ee4b03aa8ba499743dc90b1e55135135
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-07-31 21:37:07 +0200

    Update and rename docker-entrypoint.sh to docker-entrypoint-hook.sh

---
+++ docker-entrypoint-hook.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+#
+# XVFB
+#
+echo "DOCKER-ENTRYPOINT >> starting Xvfb"
+Xvfb :99 -ac -screen 0 1280x1024x16 -nolisten tcp &
+xvfb=$!
+export DISPLAY=:99
+
+#
+# DBUS
+#
+echo "DOCKER-ENTRYPOINT >> starting dbus"
+eval `dbus-launch --sh-syntax --config-file=/tmp/dbus-system.conf`
+
+#
+# PULSEAUDIO
+#
+# FIXME: "Daemon startup failed"
+#pulseaudio --daemonize
+
+#
+# CHROME
+#
+export CHROME_BIN="/usr/bin/google-chrome"
